713a053063a5f762b149cb5b68d51379c05dafab
mohammedsahal360 <149261682+mohammedsahal360@users.noreply.github.com>
2025-01-05 14:47:21 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,12 +1,12 @@
 const mysql = require('mysql2');
 
-// Fetching the MySQL connection details from environment variables
+// Fetching database connection details from environment variables
 const connection = mysql.createConnection({
-    host: process.env.MYSQL_HOST, // Render's MySQL host URL (provided by Render)
-    user: process.env.MYSQL_USER, // Your MySQL username (provided by Render)
-    password: process.env.MYSQL_PASSWORD, // Your MySQL password (provided by Render)
-    database: process.env.MYSQL_DATABASE, // Your database name (provided by Render)
-    port: process.env.MYSQL_PORT || 3306, // Default MySQL port, 3306
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    port: process.env.DATABASE_PORT,
 });
 
 connection.connect((err) => {
